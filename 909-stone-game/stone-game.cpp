ab88219a3795e4e7cdbcbf1f27e68eba5ad8c7c1
Palindrome0 <rajarsisarkar28@gmail.com>
2026-08-09 11:06:32 +0530

    Time: 7 ms (30.49%) | Memory: 19.7 MB (7.28%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        vector<vector<int>>dp(piles.size(),vector<int>(piles.size(),0));
+        for(int i=0;i<piles.size();i++){
+            dp[i][i]=piles[i];
+        }
+        for(int i=piles.size()-1;i>=0;i--){
+            for(int j=0;j<piles.size();j++){
+                if(i>j) continue;
+                if(i==piles.size()-1) {dp[i][j]=piles[j]-dp[i][j-1]; continue;}
+                if(j==0) {dp[i][j]=piles[i]-dp[i+1][j]; continue;}
+                dp[i][j]=max(piles[i]-dp[i+1][j],piles[j]-dp[i][j-1]);
+            }
+        }
+        return dp[0][piles.size()-1]>0;
     }
 };
