4f9e859b99c90f3e2e70e145b410dd5dc70908f5
Palindrome0 <rajarsisarkar28@gmail.com>
2026-08-09 18:05:05 +0530

    Time: 31 ms (81.35%) | Memory: 9 MB (82.41%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        vector<bool>dp(n+1,false);
-        for(int x=1;x<=n;x++){
-            for(int k=1;k*k<=x;k++){
-                if(dp[x-k*k]==false){
-                    dp[x]=true;
+        vector<bool> dp(n + 1, false);
+        for(int x = 1; x <= n; x++) {
+            bool ans = false;
+            for(int i = 1; i * i <= x; i++) {
+                if(!dp[x - i * i]) {
+                    ans = true;
                     break;
                 }
             }
+            dp[x] = ans;
         }
         return dp[n];
     }
