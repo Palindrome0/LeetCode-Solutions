213bb7ee3ec720c1cec903a9fd2094d3071b4fa2
Palindrome0 <rajarsisarkar28@gmail.com>
2026-08-10 19:33:39 +0530

    Time: 8 ms (99.02%) | Memory: 115.6 MB (65.85%) - LeetSync

--- 1788-stone-game-vi/stone-game-vi.cpp
+++ 1788-stone-game-vi/stone-game-vi.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
-        int n=aliceValues.size();
-        vector<pair<int,int>>stones;
-        for (int i=0;i<n;i++){
-            stones.push_back({aliceValues[i]+bobValues[i],i});
+        int n = aliceValues.size();
+        vector<vector<int>> bucket(201);
+        for (int i = 0; i < n; i++) {
+            bucket[aliceValues[i] + bobValues[i]].push_back(i);
         }
-        sort(stones.rbegin(),stones.rend());
-        int alice=0;
-        int bob=0;
-        for(int turn=0;turn<n;turn++){
-            int idx=stones[turn].second;
-            if(turn%2==0){
-                alice+=aliceValues[idx];
-            } 
-            else{
-                bob+=bobValues[idx];
+        int alice = 0;
+        int bob = 0;
+        int turn = 0;
+        for (int sum = 200; sum >= 2; sum--) {
+            for (int idx : bucket[sum]) {
+                if (turn % 2 == 0)
+                    alice += aliceValues[idx];
+                else
+                    bob += bobValues[idx];
+                turn++;
             }
         }
-        if(alice>bob)
-        return 1;
-        if(alice<bob)
-        return -1;
+        if (alice > bob)
+            return 1;
+        if (alice < bob)
+            return -1;
         return 0;
     }
 };
