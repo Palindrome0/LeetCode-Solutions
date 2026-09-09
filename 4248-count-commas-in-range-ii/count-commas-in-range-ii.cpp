f15d060bc8825ccf974bef9fc35595587d97524d
Palindrome0 <rajarsisarkar28@gmail.com>
2026-09-09 11:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0ll;
+        for(long long i=1000;i<=n;i*=1000)
+            ans+=n-i+1;
+        return ans;
+    }
+};
