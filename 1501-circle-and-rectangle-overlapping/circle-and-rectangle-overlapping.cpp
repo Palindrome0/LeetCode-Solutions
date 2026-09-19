2dad7e6841fa4647d93ac15da6511d5a3cd2b1fa
Palindrome0 <rajarsisarkar28@gmail.com>
2026-09-19 11:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.38%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        bool f1=(max(xCenter-radius,x1)<=min(x2,xCenter+radius)?true:false);
-        bool f2=(max(yCenter-radius,y1)<=min(y2,yCenter+radius)?true:false);
-        if(radius==1415&&xCenter==807&&yCenter==-784&&x1==-733&&y1==623&&x2==-533&&y2==1005) 
-        return false;
-        return f1&&f2;
+        int closestX = max(x1, min(xCenter, x2));
+        int closestY = max(y1, min(yCenter, y2));
+        long long dx = closestX - xCenter;
+        long long dy = closestY - yCenter;
+        return dx * dx + dy * dy <= 1LL * radius * radius;
     }
 };
